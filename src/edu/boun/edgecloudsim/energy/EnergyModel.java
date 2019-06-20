c714919136bc490afc5196ec04298c28a6672046
Matthew Merck <mlm0175@auburn.edu>
2019-06-20 13:15:23 -0500

    Added comments to EnergyModel.java

--- src/edu/boun/edgecloudsim/energy/EnergyModel.java
+++ src/edu/boun/edgecloudsim/energy/EnergyModel.java
@@ -1,7 +1,14 @@
 package edu.boun.edgecloudsim.energy;
-
+/**
+ * Class for energy measurement
+ * Written by Matthew Merck and Cameron Berry
+ * 
+ * @author mlm0175
+ *
+ */
 public class EnergyModel {
 	
+	//All private values are values to be logged
 	private static double totalEnergy;
 
 	public static double getDownloadEnergy(long taskFileSize, double nJperBit) {
