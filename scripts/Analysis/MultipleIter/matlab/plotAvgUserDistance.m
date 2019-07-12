9a5fb9ecd7f0674182db9c13b919cb7e770a68ac
AgentEnder <craigorycoppola@gmail.com>
2019-07-12 13:17:59 -0500

    Updated class interactions graph, added remaining metric script

---
+++ scripts/Analysis/MultipleIter/matlab/plotAvgUserDistance.m
@@ -0,0 +1,9 @@
+function [] = plotAvgUserDistance()
+
+    %plotGenericResult(9, 1, 'Avg. distance (m) to user', 'ALL_APPS', 0, 'ALL APPS', 'log');
+    %plotGenericResult(9, 1, {'Avg. distance (m) to user'}, 'MACHINE_LEARNING', 0, 'MACHINE LEARNING APP', 'log');
+    %plotGenericResult(9, 1, {'Avg. distance (m) to user'}, 'REMOTE_HEALTHCARE', 0, 'REMOTE HEALTHCARE APP', 'log');
+    %plotGenericResult(9, 1, {'Avg. distance (m) to user'}, 'COGNITIVE_ASSISTANCE', 0, 'COGNITIVE ASSISTANCE APP', 'log');
+    plotGenericResult(9, 1, {'Avg. distance (m) to user'}, 'AUGMENTED_REALITY', 0, 'AUGMENTED REALITY APP', 'log');
+        
+end
