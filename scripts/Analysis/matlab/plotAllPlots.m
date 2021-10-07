b41052864724c425ec83957cbf51a44d2b3b4433
roy-harmon <harmon58@live.marshall.edu>
2021-10-06 23:01:19 -0500

    Automatically generate plots for all represented app types

--- scripts/Analysis/matlab/plotAllPlots.m
+++ scripts/Analysis/matlab/plotAllPlots.m
@@ -1,19 +1,27 @@
 function [] = plotAllPlots()
-    % Cross your fingers...
-    appType = 'ALL_APPS' % 'AUGMENTED_REALITY' %
-%    plotGenericResult(1, 8, 'Average VM Utilization (%)', appType, 0);
-%     plotGenericResult(1, 5, 'Service Time (sec)', appType, 0);
-%     plotGenericResult(2, 5, 'Service Time on Fog (sec)', appType, 0);
-%     plotGenericResult(3, 5, 'Service Time on Cloud (sec)', appType, 0);
-%     plotGenericResult(1, 6, 'Processing Time (sec)', appType, 0);
-%     plotGenericResult(2, 6, 'Processing Time on Cloudlet (sec)', appType, 0);
-%     plotGenericResult(3, 6, 'Processing Time on Cloud (sec)', appType, 0);
-%     plotGenericResult(1, 7, 'Average Network Delay (sec)', appType, 0);
-%     plotGenericResult(2, 7, 'Average WLAN Delay (sec)', appType, 0);
-%     plotGenericResult(3, 7, 'Average WAN Delay (sec)', appType, 0);
-%     plotGenericResult(4, 2, 'Average Hops', appType, 0);
-%     plotGenericResult(1, 2, 'Failed Tasks (%)', appType, 1);
-%     plotGenericResult(4, 1, 'Average Distance (m)', appType, 0);
-     plotGenericResult(1, 9, 'Average Cost ($)', appType, 0);
+    folderPath = getConfiguration(1);
+    allFiles = dir(strcat(folderPath,'\\*DEVICES_*_GENERIC*'));
+    regex = 'DEVICES_([\w_\s]+)_GENERIC';
+    allNames = [allFiles.name];
+    allTypesList = regexp(allNames, regex, 'tokens');
+    allTypes = string(allTypesList);
+    typesList = unique(allTypes(:));
+    for i=1:length(typesList)
+        appType = allTypes(i);
+        plotGenericResult(1, 8, 'Average VM Utilization (%)', appType, 0);
+        plotGenericResult(1, 5, 'Service Time (sec)', appType, 0);
+        plotGenericResult(2, 5, 'Service Time on Fog (sec)', appType, 0);
+        plotGenericResult(3, 5, 'Service Time on Cloud (sec)', appType, 0);
+        plotGenericResult(1, 6, 'Processing Time (sec)', appType, 0);
+        plotGenericResult(2, 6, 'Processing Time on Cloudlet (sec)', appType, 0);
+        plotGenericResult(3, 6, 'Processing Time on Cloud (sec)', appType, 0);
+        plotGenericResult(1, 7, 'Average Network Delay (sec)', appType, 0);
+        plotGenericResult(2, 7, 'Average WLAN Delay (sec)', appType, 0);
+        plotGenericResult(3, 7, 'Average WAN Delay (sec)', appType, 0);
+        plotGenericResult(4, 2, 'Average Hops', appType, 0);
+        plotGenericResult(1, 2, 'Failed Tasks (%)', appType, 1);
+        plotGenericResult(4, 1, 'Average Distance (m)', appType, 0);
+        plotGenericResult(1, 9, 'Average Cost ($)', appType, 0);
 %     %plotLocation();
+    end
 end
